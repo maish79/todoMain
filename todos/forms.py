d8c2ad38714c0a2021f11c9588a72885f66928af
maish79 <maishsupa@yahoo.com>
2022-12-25 20:43:17 +0000

    add bootstrap and css style

--- todos/forms.py
+++ todos/forms.py
@@ -4,6 +4,7 @@ from django.forms import ModelForm
 from .models import *
 
 class listForm(forms.ModelForm):
+    title= forms.CharField(widget= forms.TextInput(attrs={'placeholder':'Add new list...'}))
 
     class Meta:
         model = list
