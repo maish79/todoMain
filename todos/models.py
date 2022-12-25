5e6842e123d38400d136fdd45a034e11f0076161
maish79 <maishsupa@yahoo.com>
2022-12-25 14:58:41 +0000

    create templates and modified models.py

--- todos/models.py
+++ todos/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+
+# create your models here.
+
+class list(models.Model):
+    title = models.CharField(max_length=100)
+    completed = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+
+
